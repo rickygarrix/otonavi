e369680a9fce6d8f6b41ba590fa14b3ed1341172
Nishimoto Hiroaki <129836027+rickygarrix@users.noreply.github.com>
2026-01-10 21:28:06 +0900

    Anonkey修正

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,18 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const serviceRoleKey = process.env.SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL
+const serviceRoleKey = process.env.SERVICE_ROLE_KEY
 
 if (!supabaseUrl) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_URL is missing");
+  throw new Error("SUPABASE_URL is missing")
 }
 
 if (!serviceRoleKey) {
-  throw new Error("SERVICE_ROLE_KEY is missing");
+  throw new Error("SERVICE_ROLE_KEY is missing")
 }
 
 export const supabase = createClient(supabaseUrl, serviceRoleKey, {
-  auth: {
-    persistSession: false,
-  },
-});
+  auth: { persistSession: false },
+})
