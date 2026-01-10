a5e42109399e263acfef89750f4d4ce38c590916
Takumi Takaishi <takumi@Takumi-Air.local>
2026-01-10 11:59:50 +0900

    BackToHomeButtonのスタイル

--- src/components/ui/BackToHomeButton.tsx
+++ src/components/ui/BackToHomeButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Search } from 'lucide-react';
+
 type Props = {
   onClick: () => void;
 };
@@ -8,9 +10,9 @@ export default function BackToHomeButton({ onClick }: Props) {
   return (
     <button
       onClick={onClick}
-      className="flex w-full items-center justify-center gap-2 rounded-2xl border border-slate-300 py-3 text-lg font-medium text-slate-700 transition hover:bg-slate-50"
+      className="border-light-5 from-light-2 to-light-1 text-dark-5 shadow-dark-1/20 flex h-12 w-full items-center justify-center gap-2 rounded-lg border bg-linear-to-t px-4 text-sm shadow-xs transition active:scale-102 active:shadow-md"
     >
-      <span className="text-xl">🔍</span>
+      <Search className="h-4 w-4" strokeWidth={1.2} />
       別の条件で探す
     </button>
   );
