2c39015a80a9aeba7023f9d1809cdb81c0a18af4
Takumi Takaishi <takumi@Takumi-Air.local>
2026-01-22 11:40:38 +0900

    hero

---
+++ src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Zen_Maru_Gothic } from 'next/font/google';
+
+export const zenMaru = Zen_Maru_Gothic({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-zen-maru',
+  display: 'swap',
+});
