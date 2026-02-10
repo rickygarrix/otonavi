4486b093782fae236381bfd5132e9f6caaee0755
Takumi Takaishi <takumi@Takumi-Air.local>
2026-02-10 09:51:06 +0900

    BackToHomeButtonのボタンを差し替え

--- src/components/ui/BackToHomeButton.tsx
+++ src/components/ui/BackToHomeButton.tsx
@@ -2,6 +2,7 @@
 
 import { Search } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import Button from '@/components/ui/Button';
 
 export default function BackToHomeButton() {
   const router = useRouter();
@@ -18,20 +19,11 @@ export default function BackToHomeButton() {
   };
 
   return (
-    <button
-      type="button"
+    <Button
       onClick={handleClick}
-      className="
-        border-light-5 from-light-2 to-light-1 text-dark-5
-        shadow-dark-1/20
-        flex h-12 w-full items-center justify-center gap-2
-        rounded-lg border bg-linear-to-t px-4 text-sm
-        shadow-xs transition
-        active:scale-102 active:shadow-md
-      "
-    >
-      <Search className="h-4 w-4" strokeWidth={1.2} />
-      別の条件で探す
-    </button>
+      priority="secondary"
+      label="別の条件で探す"
+      leftIcon={Search}
+    ></Button>
   );
 }
